3cc106b8228d70dc671bc50edc4a1a6a86b61acb
Charles V <charlesvaldezz94@gmail.com>
2023-02-15 21:53:59 +0000

    all tests passed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,14 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+        if( bool ) {
+            return x;
+        };
+        if(bool == false ) {
+            return y;
+        }
+        else {
+            return 0;
+        }
     }
 }
